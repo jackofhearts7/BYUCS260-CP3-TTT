ef8110274e2563fd89379404acd7d98495aa357c
DallinFromEarth <112426674+DallinFromEarth@users.noreply.github.com>
2022-10-31 17:21:08 -0600

    center board, keep board size constant, fix background

--- src/board.js
+++ src/board.js
@@ -124,23 +124,23 @@ const Board = ({ reset, setReset, winner, setWinner }) => {
 
 	return (
 		<div ref={boardRef} className="board">
-			<div className="input input-1"
+			<div class="input" id="input-1"
 				onClick={(e) => draw(e, 1)}></div>
-			<div className="input input-2"
+			<div class="input" id="input-2"
 				onClick={(e) => draw(e, 2)}></div>
-			<div className="input input-3"
+			<div class="input" id="input-3"
 				onClick={(e) => draw(e, 3)}></div>
-			<div className="input input-4"
+			<div class="input" id="input-4"
 				onClick={(e) => draw(e, 4)}></div>
-			<div className="input input-5"
+			<div class="input" id="input-5"
 				onClick={(e) => draw(e, 5)}></div>
-			<div className="input input-6"
+			<div class="input" id="input-6"
 				onClick={(e) => draw(e, 6)}></div>
-			<div className="input input-7"
+			<div class="input" id="input-7"
 				onClick={(e) => draw(e, 7)}></div>
-			<div className="input input-8"
+			<div class="input" id="input-8"
 				onClick={(e) => draw(e, 8)}></div>
-			<div className="input input-9"
+			<div class="input" id="input-9"
 				onClick={(e) => draw(e, 9)}></div>
 		</div>
 	)
